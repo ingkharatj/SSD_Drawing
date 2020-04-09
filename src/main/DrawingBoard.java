8ba48a986ce5bdddecb00893743f0d6db088a36a
Thaksina <thaksina.j@ku.th>
2020-04-09 17:44:52 +0700

    fixed button

--- src/main/DrawingBoard.java
+++ src/main/DrawingBoard.java
@@ -10,127 +10,129 @@ import objects.*;
 
 public class DrawingBoard extends JPanel {
 
-	private MouseAdapter mouseAdapter; 
-	private List<GObject> gObjects;
-	private GObject target;
-	
-	private int gridSize = 10;
-	
-	public DrawingBoard() {
-		gObjects = new ArrayList<GObject>();
-		mouseAdapter = new MAdapter();
-		addMouseListener(mouseAdapter);
-		addMouseMotionListener(mouseAdapter);
-		setPreferredSize(new Dimension(800, 600));
-	}
-	
-	public void addGObject(GObject gObject) {
-		gObjects.add(gObject);
-		repaint();
-	}
-	
-	public void groupAll() {
+    private MouseAdapter mouseAdapter;
+    private List<GObject> gObjects;
+    private GObject target;
 
-		CompositeGObject comp_Object = new CompositeGObject();
+    private int gridSize = 10;
 
-		for (GObject gObject : gObjects){
-			comp_Object.add(gObject);
-		}
-		gObjects.clear();
-		comp_Object.recalculateRegion();
-		gObjects.add(comp_Object);
+    public DrawingBoard() {
+        gObjects = new ArrayList<GObject>();
+        mouseAdapter = new MAdapter();
+        addMouseListener(mouseAdapter);
+        addMouseMotionListener(mouseAdapter);
+        setPreferredSize(new Dimension(800, 600));
+    }
 
-		repaint();
+    public void addGObject(GObject gObject) {
+        gObjects.add(gObject);
+        repaint();
+    }
 
-		
-	}
+    public void groupAll() {
 
-	public void deleteSelected() {
-		gObjects.remove(target);
+        CompositeGObject comp_Object = new CompositeGObject();
 
-		repaint();
+        for (GObject gObject : gObjects){
+            comp_Object.add(gObject);
+        }
+        gObjects.clear();
+        comp_Object.recalculateRegion();
+        gObjects.add(comp_Object);
 
-	}
-	
-	public void clear() {
-
-		gObjects.clear();
-	}
-	
-	@Override
-	public void paint(Graphics g) {
-		super.paint(g);
-		paintBackground(g);
-		paintGrids(g);
-		paintObjects(g);
-	}
-
-	private void paintBackground(Graphics g) {
-		g.setColor(Color.white);
-		g.fillRect(0, 0, getWidth(), getHeight());
-	}
-
-	private void paintGrids(Graphics g) {
-		g.setColor(Color.lightGray);
-		int gridCountX = getWidth() / gridSize;
-		int gridCountY = getHeight() / gridSize;
-		for (int i = 0; i < gridCountX; i++) {
-			g.drawLine(gridSize * i, 0, gridSize * i, getHeight());
-		}
-		for (int i = 0; i < gridCountY; i++) {
-			g.drawLine(0, gridSize * i, getWidth(), gridSize * i);
-		}
-	}
-
-	private void paintObjects(Graphics g) {
-		for (GObject go : gObjects) {
-			go.paint(g);
-		}
-	}
-
-	class MAdapter extends MouseAdapter {
-
-		int x = 0;
-		int y = 0;
-	
-		private void deselectAll() {
-
-			for(GObject gObject : gObjects){
-				gObject.deselected();
-				target = null;
-			}
-			repaint();
-		}
-		
-		@Override
-		public void mousePressed(MouseEvent e) {
-
-			this.x = e.getX();
-			this.y = e.getY();
-
-			deselectAll();
-
-			for(GObject gObject : gObjects){
-				if (gObject.pointerHit(x, y)){
-					target = gObject;
-					target.selected();
-				}
-			}
-			repaint();
+        repaint();
 
 
-		}
+    }
 
-		@Override
-		public void mouseDragged(MouseEvent e) {
+    public void deleteSelected() {
+        gObjects.remove(target);
 
-			if(target != null){
-				target.move(e.getX() - x, e.getY() - y);
-				x = e.getX();
-				y = e.getY();
-			}
-			repaint();
-		}
-	}
-	
+        repaint();
+
+    }
+
+    public void clear() {
+
+        gObjects.clear();
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        paintBackground(g);
+        paintGrids(g);
+        paintObjects(g);
+    }
+
+    private void paintBackground(Graphics g) {
+        g.setColor(Color.white);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+    private void paintGrids(Graphics g) {
+        g.setColor(Color.lightGray);
+        int gridCountX = getWidth() / gridSize;
+        int gridCountY = getHeight() / gridSize;
+        for (int i = 0; i < gridCountX; i++) {
+            g.drawLine(gridSize * i, 0, gridSize * i, getHeight());
+        }
+        for (int i = 0; i < gridCountY; i++) {
+            g.drawLine(0, gridSize * i, getWidth(), gridSize * i);
+        }
+    }
+
+    private void paintObjects(Graphics g) {
+        for (GObject go : gObjects) {
+            go.paint(g);
+        }
+    }
+
+    class MAdapter extends MouseAdapter {
+
+        int x = 0;
+        int y = 0;
+
+        private void deselectAll() {
+
+            for(GObject gObject : gObjects){
+                gObject.deselected();
+
+            }
+            target = null;
+        }
+
+        @Override
+        public void mousePressed(MouseEvent e) {
+
+
+            this.x = e.getX();
+            this.y = e.getY();
+
+            deselectAll();
+
+            for(GObject gObject : gObjects){
+                if (gObject.pointerHit(x, y)){
+                    target = gObject;
+                    target.selected();
+                    break;
+                }
+            }
+            repaint();
+
+
+        }
+
+        @Override
+        public void mouseDragged(MouseEvent e) {
+
+            if(target != null){
+                target.move(e.getX() - x, e.getY() - y);
+                x = e.getX();
+                y = e.getY();
+            }
+            repaint();
+        }
+    }
+
 }
